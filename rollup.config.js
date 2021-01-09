1780c0f88aa7d3939a86cf34ec308e1f9c7aaf15
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-01-09 19:52:10 +0100

    improvments demo and build tasks

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,45 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy';
 import { terser } from 'rollup-plugin-terser';
+import replaceHtmlVars from 'rollup-plugin-replace-html-vars';
 
 export default {
   preserveSymlinks: true,
-	input: ['animation-starwarstext.js'],
-	output: {
-		file: 'dist/animation-starwarstext.js',
+  input: ['./animation-starwarstext.js'],
+  output: {
+    dir: './dist/js',
     format: 'es',
-		sourcemap: true
-	},
-	plugins: [
+    sourcemap: true,
+  },
+  plugins: [
     resolve(),
     babel(),
-    terser()
-  ]
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+    copy({
+      targets: [{
+        src: './animation-starwarstext.js',
+        dest: './dist/js',
+      },{
+        src: './demo/css/styles.css',
+        dest: './dist/css',
+      },{
+        src: './demo/img/*',
+        dest: './dist/img',
+      },{
+        src: './demo/index.html',
+        dest: './dist',
+      }],
+    }),
+    replaceHtmlVars({
+      files: './demo/index.html',
+      from: '<script type="module" src="..',
+      to: '<script type="module" src="js',
+    }),
+  ],
 };
+
